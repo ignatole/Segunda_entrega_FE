035ebc0194b0e36d2852ee640e0703d3722ea2de
Romina <rominaggargano@gmail.com>
2025-10-16 03:30:56 -0300

    integrantes cards con json y efectos

--- Segunda-Entrega-FE/src/pages/portada/Portada.jsx
+++ Segunda-Entrega-FE/src/pages/portada/Portada.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import './Portada.css';
 
 
@@ -11,7 +11,9 @@ export default function Portada(){
   ];
 
   const [index, setIndex] = useState(0);
-
+  const titleRef = useRef(null);
+  
+  // fondos 
   useEffect(() => {
     const intervalo = setInterval(() => {
       setIndex((prev) => (prev + 1) % fondos.length);
@@ -21,13 +23,32 @@ export default function Portada(){
     return () => clearInterval(intervalo);
   }, []);
 
+  // animacion titulo
+  useEffect(() => {
+    const node = titleRef.current;
+    if(!node) return;
+
+    const text = node.innerText;
+    node.innerText = '';
+    
+    const letters = [...text].map((char, i) => {
+    const span = document.createElement('span');
+    span.textContent = char;
+    span.style.setProperty('--index', i);
+    return span;
+  });
+
+  node.append(...letters);
+  }, []);
+
   return (
     <div className="portada"
     style={{backgroundImage: `url(${fondos[index]})`}}>
       <div className="overlay">
-        <h1 className="titulo">Portalverse</h1>
+        <h1 ref={titleRef} letter-animation= "breath">Portalverse</h1>
         <p className="subtitulo">Explorando el multiverso del desarrollo web</p>
-        <p className="descripcion">Somos un grupo de estudiantes de Tecnicatura en Desarrollo Web </p>
+        <p className="descripcion">Bienvenidos a un viaje interdimensional por los mundos del diseño, la programación
+          y la creatividad infinita. </p>
     </div>
     </div>
   );
