55b323bd0811c06faedf4aa54fc1cc2a2c8713da
Romina <rominaggargano@gmail.com>
2025-10-20 22:07:11 -0300

    correccion +

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import './Sidebar.css'
+import './SideBar.css'
 
 export default function SideBar({ onNavigate })  {
     const [collapsed, setCollapsed] = useState(false);
